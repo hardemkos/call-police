a9a7b44a7bfe7b2fcbb9978136f6a60e9ac544e7
hardemko <ssachardemko@gmail.com>
2026-03-13 20:39:31 +0100

    Update client.lua

--- policecall/client.lua
+++ policecall/client.lua
@@ -2,24 +2,24 @@ local ox_target = exports['ox_target']
 
 Citizen.CreateThread(function()
     -- Koordináty policejní stanice
-    local policeStationCoords = vector3(441.2609, -981.9473, 30.6895)  -- Změň podle tvé skutečné policejní stanice
-    local range = 2.0  -- Vzdálenost, na kterou se objeví interakce
+    local policeStationCoords = vector3(441.2609, -981.9473, 30.6895)  
+    local range = 2.0 
 
-    -- Přidání interakce pomocí ox_target
+
     ox_target:addSphereZone({
-        coords = policeStationCoords,  -- Koordináty policejní stanice
-        radius = range,  -- Velikost oblasti pro interakci
-        debug = false,  -- Vypnutí debugování
+        coords = policeStationCoords,  
+        radius = range,  
+        debug = false,  
         options = {
             {
-                name = "call_for_help",  -- Název interakce
-                label = "Zavolej pomoc",  -- Text na obrazovce
-                icon = "fa-solid fa-phone",  -- Ikona pro tuto akci
+                name = "call_for_help",  
+                label = "Zavolej pomoc",  
+                icon = "fa-solid fa-phone",  
                 onSelect = function()
-                    -- Získání hráčových informací
+                  
                     local data = exports['cd_dispatch']:GetPlayerInfo()
                     
-                    -- Zavolání serverového eventu pro odeslání notifikace
+                    
                     TriggerServerEvent('police:callForHelp', data.coords, data.sex, data.street, data.unique_id)
                 end,
             }
